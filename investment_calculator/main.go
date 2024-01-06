d8f7772e8fc82c0abe0c282068291e3aacd916f7
danangamw <danangamw@gmail.com>
2024-01-06 11:26:31 +0700

    GO essentials done

--- investment_calculator/main.go
+++ investment_calculator/main.go
@@ -5,24 +5,45 @@ import (
 	"math"
 )
 
+const inflationRate float64 = 2.5
+
 func main() {
-	const inflationRate float64 = 2.5
+
 	var investmentAmount float64
-	var expectedReturnRate float64
+	expectedReturnRate := 5.5
 	var years float64
 
-	fmt.Print("Investment Amount: ")
+	outputText("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected Return Rate: ")
+	outputText("Expected Return Rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	fmt.Print("Years: ")
+	outputText("Years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
+	futureValue, futureRealValue := calculateFutureValue(investmentAmount, expectedReturnRate, years)
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
+	formattedFRV := fmt.Sprintf("Future Value (adjusted for inflation: %.2f\n", futureRealValue)
+
+	// outputs information
+	// fmt.Println("Future Value: ", futureValue)
+	// fmt.Printf("Future Value: %.2f\nFuture Value (adjusted for inflation: %.2f\n", futureValue, futureRealValue)
+	// fmt.Printf(`Future Value: %.2f\nFuture Value (adjusted for inflation: %.2f\n`, futureValue, futureRealValue)
+	fmt.Print(formattedFV, formattedFRV)
+
+}
+
+func outputText(text string) {
+	fmt.Print(text)
+}
+
+func calculateFutureValue(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
+	rfv = fv / math.Pow(1+inflationRate/100, float64(years))
+	// return fv, rfv
+	return
 }
